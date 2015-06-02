49f89a4566cb744feeffc1ddb9d3344d60558b24
Ole <ole@vprsm.de>
2015-06-02 15:35:53 +0200

    Use URIBuilder to generate request URI

--- src/test/java/org/fiteagle/adapters/containers/Playground.java
+++ src/test/java/org/fiteagle/adapters/containers/Playground.java
@@ -3,14 +3,19 @@ package org.fiteagle.adapters.containers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.*;
 
 public class Playground {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, URISyntaxException {
 		CloseableHttpClient client = HttpClients.createDefault();
-		HttpGet request = new HttpGet("http://localhost:8080/version");
+		
+		URI uri = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080).setPath("/version").build();
+		HttpGet request = new HttpGet(uri);
 
 		try {
 			CloseableHttpResponse response = client.execute(request);
@@ -22,8 +27,7 @@ public class Playground {
 				System.out.println(line);
 			}
 		} catch (Exception e) {
-			System.out.println("Exception while performing HTTP request:");
-			System.out.println(e);
+			e.printStackTrace();
 		} finally {
 			client.close();
 		}
