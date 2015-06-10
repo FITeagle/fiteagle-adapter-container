bb95aa3ea25532c1deedb008d0562decd973ecc2
Ole Krüger <ole@vprsm.de>
2015-06-10 14:45:55 +0200

    First draft for a request builder

--- src/main/java/org/fiteagle/adapters/containers/docker/internal/RequestBuilder.java
+++ src/main/java/org/fiteagle/adapters/containers/docker/internal/RequestBuilder.java
@@ -1,12 +1,18 @@
 package org.fiteagle.adapters.containers.docker.internal;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.apache.http.client.utils.URIBuilder;
+
 public class RequestBuilder {
 	private String scheme = "http";
 	private String hostname;
 	private int port = 80;
 	
 	/**
-	 * Instatiate a request builder which targets an endpoint at `scheme://hostname:port`.
+	 * Instantiate a request builder which targets an endpoint at `scheme://hostname:port`.
 	 */
 	public RequestBuilder(String scheme, String hostname, int port) {
 		this.scheme = scheme;
@@ -15,7 +21,7 @@ public class RequestBuilder {
 	}
 	
 	/**
-	 * Instatiate a request builder which targets an endpoint at `scheme://hostname:80`.
+	 * Instantiate a request builder which targets an endpoint at `scheme://hostname:80`.
 	 */
 	public RequestBuilder(String scheme, String hostname) {
 		this.scheme = scheme;
@@ -23,7 +29,7 @@ public class RequestBuilder {
 	}
 	
 	/**
-	 * Instatiate a request builder which targets an endpoint at `http://hostname:port`.
+	 * Instantiate a request builder which targets an endpoint at `http://hostname:port`.
 	 */
 	public RequestBuilder(String hostname, int port) {
 		this.hostname = hostname;
@@ -31,9 +37,42 @@ public class RequestBuilder {
 	}
 	
 	/**
-	 * Instatiate a request builder which targets an endpoint at `http://hostname:80`.
+	 * Instantiate a request builder which targets an endpoint at `http://hostname:80`.
 	 */
 	public RequestBuilder(String hostname) {
 		this.hostname = hostname;
+	}
+	
+	/**
+	 * Create and prepare a URLBuilder.
+	 */
+	private URIBuilder prepareBuilder() {
+		return
+			new URIBuilder()
+				.setScheme(scheme)
+				.setHost(hostname)
+				.setPort(port);
+	}
+	
+	/**
+	 * Build the URL for a version request.
+	 */
+	public URL version() throws MalformedURLException, URISyntaxException {
+		return
+			prepareBuilder()
+				.setPath("/version")
+				.build()
+				.toURL();
+	}
+	
+	/**
+	 * Build the URL for a information request.
+	 */
+	public URL info() throws MalformedURLException, URISyntaxException {
+		return
+			prepareBuilder()
+				.setPath("/info")
+				.build()
+				.toURL();
 	}
 }
