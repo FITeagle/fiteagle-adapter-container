5a9e410d0283a75b99c744a803d62d3702e4072b
Ole Krüger <ole@vprsm.de>
2015-06-10 14:46:03 +0200

    Purge Playground

--- src/test/java/org/fiteagle/adapters/containers/Playground.java
+++ src/test/java/org/fiteagle/adapters/containers/Playground.java
@@ -4,32 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 
-import org.apache.http.client.methods.*;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.*;
+
 
 public class Playground {
-	public static void main(String[] args) throws IOException, URISyntaxException {
-		CloseableHttpClient client = HttpClients.createDefault();
+	public static void main(String[] args) throws IOException {
+//		URL url = new URL("http://localhost:8080/version");
+		URL url = new URL("http", "localhost", 8080, "/containers/json?all=1");
 		
-		URI uri = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080).setPath("/version").build();
-		HttpGet request = new HttpGet(uri);
-
-		try {
-			CloseableHttpResponse response = client.execute(request);
-			BufferedReader breader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-
-			String line = null;
-
-			while ((line = breader.readLine()) != null) {
-				System.out.println(line);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			client.close();
+		URLConnection connection = url.openConnection();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		
+		String line;
+		while ((line = reader.readLine()) != null) {
+			System.out.println(line);
 		}
+		
+//		CloseableHttpClient client = HttpClients.createDefault();
+//			
+//		URI uri = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080).setPath("/version").build();
+//		HttpGet request = new HttpGet(uri);
+//
+//		try {
+//			CloseableHttpResponse response = client.execute(request);
+//			BufferedReader breader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+//
+//			String line = null;
+//
+//			while ((line = breader.readLine()) != null) {
+//				System.out.println(line);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			client.close();
+//		}
 	}
 }
