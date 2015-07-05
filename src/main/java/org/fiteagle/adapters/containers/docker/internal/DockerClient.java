2979f5a069733c834e9f26cfcba04c91df598044
Ole <ole@vprsm.de>
2015-07-05 18:55:54 +0200

    Integrate functionality into DockerClient

---
+++ src/main/java/org/fiteagle/adapters/containers/docker/internal/DockerClient.java
@@ -0,0 +1,41 @@
+package org.fiteagle.adapters.containers.docker.internal;
+
+import java.util.LinkedList;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class DockerClient {
+	private RequestBuilder requestBuilder;
+	private CloseableHttpClient httpClient;
+
+	public DockerClient(String hostname, int port) {
+		requestBuilder = new RequestBuilder(hostname, port);
+		httpClient = HttpClients.createDefault();
+	}
+
+
+	public String createContainer(CreateContainerInfo cci)
+		throws DockerException
+	{
+		try {
+			return ResponseParser.createContainer(
+				httpClient.execute(requestBuilder.createContainer(cci))
+			);
+		} catch (Exception e) {
+			throw new DockerException(e);
+		}
+	}
+
+	public LinkedList<ContainerInfo> listContainers(boolean all)
+		throws DockerException
+	{
+		try {
+			return ResponseParser.listContainers(
+				httpClient.execute(requestBuilder.listContainers(all))
+			);
+		} catch (Exception e) {
+			throw new DockerException(e);
+		}
+	}
+}
--- src/test/java/org/fiteagle/adapters/containers/Playground.java
+++ src/test/java/org/fiteagle/adapters/containers/Playground.java
@@ -1,40 +1,25 @@
 package org.fiteagle.adapters.containers;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.LinkedList;
-
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.fiteagle.adapters.containers.docker.internal.ContainerInfo;
+import org.fiteagle.adapters.containers.docker.internal.CreateContainerInfo;
+import org.fiteagle.adapters.containers.docker.internal.DockerClient;
 import org.fiteagle.adapters.containers.docker.internal.DockerException;
-import org.fiteagle.adapters.containers.docker.internal.RequestBuilder;
-import org.fiteagle.adapters.containers.docker.internal.ResponseParser;
 
 public class Playground {
 	public static void main(String[] args)
-			throws URISyntaxException, ClientProtocolException, IOException, DockerException
+		throws DockerException
 	{
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		RequestBuilder requestBuilder = new RequestBuilder("localhost", 8080);
+		DockerClient client = new DockerClient("localhost", 8080);
+
+		// Create container
+		String containerID =
+			client.createContainer(new CreateContainerInfo("my_container", "ubuntu"));
+
+		System.out.println("Created '" + containerID + "'");
 
 		// List containers
-		CloseableHttpResponse listResponse = httpClient.execute(
-			requestBuilder.listContainers(true)
-		);
-
-		LinkedList<ContainerInfo> containers = ResponseParser.listContainers(listResponse);
-
-		System.out.println("Got " + containers.size() + " containers");
-
-		for (ContainerInfo ci: containers) {
-			System.out.println(ci.id + ": " + ci.image);
+		for (ContainerInfo ci: client.listContainers(true)) {
+			System.out.println("Found container: '" + ci.id + "' (" + ci.image + ")");
 		}
-
-		// Finalize
-		listResponse.close();
-		httpClient.close();
 	}
 }
