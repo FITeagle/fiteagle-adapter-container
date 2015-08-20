55296dfeaccea93dcb4aad7500cf3f93e7dfe049
Ole <ole@vprsm.de>
2015-08-20 14:12:20 +0200

    Configure for each update request

--- src/main/java/org/fiteagle/adapters/containers/docker/DockerContainer.java
+++ src/main/java/org/fiteagle/adapters/containers/docker/DockerContainer.java
@@ -30,7 +30,6 @@ public class DockerContainer {
 
 	private enum State {
 		Dead,       // Empty
-		Configured, // Configuration created
 		Failed,     // Create/Start failed
 		Active      // Create/Start succeeded
 	}
@@ -54,6 +53,8 @@ public class DockerContainer {
 	public void update(Resource newState) {
 		logger.info("Received update request");
 
+		configureFromResource(newState);
+
 		if (newState == null || !newState.hasProperty(Omn_lifecycle.hasState)) {
 			logger.severe("Invalid update model");
 			return;
@@ -63,84 +64,63 @@ public class DockerContainer {
 		Resource stateResource =
 			newState.getProperty(Omn_lifecycle.hasState).getObject().asResource();
 
-		if (stateResource.equals(Omn_lifecycle.Ready)) {
-			handleReady(newState);
-		} else if (stateResource.equals(Omn_lifecycle.Uncompleted)) {
-			handleUncompleted(newState);
+		if (stateResource.equals(Omn_lifecycle.Active)) {
+			containerState = State.Active;
+		} else if (stateResource.equals(Omn_lifecycle.Failure)) {
+			containerState = State.Failed;
 		} else {
-			logger.severe("Unsupported lifecycle state: " + stateResource.toString());
-		}
-	}
-
-	private void handleUncompleted(Resource newState) {
-		if (containerState == State.Active)
-			delete();
-
-		configureFromResource(newState);
-	}
-
-	private void handleReady(Resource newState) {
-		handleUncompleted(newState);
-
-		if (containerState == State.Configured && containerConf != null) {
-			try {
-				logger.info("Starting with configuration " + containerConf.toJsonObject().toString());
-				containerID = adapter.getDockerClient().create(containerConf);
-
-				if (containerID != null && adapter.getDockerClient().start(containerID))
-					containerState = State.Active;
-				else
-					containerState = State.Failed;
-			} catch (DockerException e) {
-				logger.throwing(DockerClient.class.getName(), "create/start", e);
-				containerState = State.Failed;
-			}
+			containerState = State.Dead;
 		}
 
-		logger.info("State = " + containerState.toString());
+		handleState();
 	}
 
-	public void delete() {
+	private void handleState() {
 		switch (containerState) {
 			case Dead:
-				break;
-
-			case Configured:
-				if (containerConf == null)
-					containerState = State.Dead;
-
+				delete();
 				break;
 
 			case Failed:
-				if (containerConf == null)
-					// An unconfigured container which failed to start? No shit.
-					containerState = State.Dead;
-				else
-					// Start failure might be caused by invalid configuration, we'll keep it anyway
-					containerState = State.Configured;
-
 				break;
 
 			case Active:
 				if (containerID != null) {
-					try {
-						adapter.getDockerClient().delete(containerID, true, true);
-					} catch (DockerException e) {
-						logger.throwing(DockerClient.class.getName(), "stop", e);
-					}
+					delete();
 				}
 
-				// Figure out if the container may stay configured
-				if (containerConf != null)
-					containerState = State.Configured;
-				else
+				if (containerConf == null) {
 					containerState = State.Dead;
+					break;
+				}
+
+				try {
+					containerID = adapter.getDockerClient().create(containerConf);
+					containerState =
+						adapter.getDockerClient().start(containerID)
+							? State.Active
+							: State.Failed;
+				} catch (DockerException e) {
+					logger.throwing(DockerClient.class.getName(), "create/start", e);
+					containerState = State.Failed;
+				}
 
 				break;
 		}
+	}
+
+	public void delete() {
+		if (containerID != null) {
+			try {
+				adapter.getDockerClient().delete(containerID, true, true);
+			} catch (DockerException e) {
+				logger.throwing(DockerClient.class.getName(), "delete", e);
+			}
+		}
 
 		containerID = null;
-		logger.info("State = " + containerState.toString());
+		containerConf = null;
+		containerState = State.Dead;
 	}
 
 	private void configureFromResource(Resource newState) {
@@ -183,46 +163,37 @@ public class DockerContainer {
 				logger.warning("Ignoring port map '" + value + "'");
 			}
 		}
-
-		containerState = State.Configured;
 	}
 
 	public Model serializeModel() {
 		Model responseModel = ModelFactory.createDefaultModel();
 		Resource resource = responseModel.createResource(instanceIdentifier);
 
-        resource.addProperty(RDF.type, instanceResource);
-        resource.addProperty(RDF.type, Omn.Resource);
-        resource.addProperty(RDFS.label, resource.getLocalName());
+		resource.addProperty(RDF.type, instanceResource);
+		resource.addProperty(RDF.type, Omn.Resource);
+		resource.addProperty(RDFS.label, resource.getLocalName());
 
-        // Add Omn-related property
-        Property property = resource.getModel().createProperty(
-    		Omn_lifecycle.hasState.getNameSpace(),
-    		Omn_lifecycle.hasState.getLocalName()
-    	);
-        property.addProperty(RDF.type, OWL.FunctionalProperty);
+		// Add Omn-related property
+		Property property = resource.getModel().createProperty(
+			Omn_lifecycle.hasState.getNameSpace(),
+			Omn_lifecycle.hasState.getLocalName()
+		);
+		property.addProperty(RDF.type, OWL.FunctionalProperty);
 
-//        switch (containerState) {
-//	        case Dead:
-//	    		resource.addProperty(property, Omn_lifecycle.NotReady);
-//	    		break;
-//
-//	        case Failed:
-//	    		resource.addProperty(property, Omn_lifecycle.Failure);
-//	    		break;
-//
-//	        case Configured:
-//	    		resource.addProperty(property, Omn_lifecycle.Provisioned);
-//	    		break;
-//
-//	        case Active:
-//	        	resource.addProperty(property, Omn_lifecycle.Active);
-//	        	break;
-//        }
+		switch (containerState) {
+			case Dead:
+				resource.addProperty(property, Omn_lifecycle.Uncompleted);
+				break;
 
-        resource.addProperty(property, Omn_lifecycle.Ready);
+			case Failed:
+				// TODO: Attach error message
+				resource.addProperty(property, Omn_lifecycle.Failure);
+				break;
 
-        // TODO: Serialize properties
+			case Active:
+				resource.addProperty(property, Omn_lifecycle.Active);
+				break;
+		}
 
 		return responseModel;
 	}
